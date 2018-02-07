aa8134ea18b5f868958eb033d9a3a1a4e8f1a9c9
Douglas G. Moore <doug@dglmoore.com>
2018-02-07 16:13:05 -0700

    Create the AbstractNeuralNetwork and SimpleANN types

--- src/Annette.jl
+++ src/Annette.jl
@@ -2,7 +2,9 @@ __precompile__(true)
 
 module Annette
 
-export AbstractNetwork, AbstractAutomaton, fire, fire!, ECA
+export AbstractNetwork, fire, fire!
+export AbstractNeuralNetwork, SimpleANN, insize, outsize
+export AbstractAutomaton, ECA
 
 """
     AbstractNetwork
@@ -31,15 +33,44 @@ function fire!(net::AbstractAutomaton{T,N}, state::AbstractArray{T,N}) where {T,
 end
 
 """
-    fire(ca::AbstractAutomaton{T,N}, state::AbstractArray{T,N})
+    fire(net::AbstractNetwork, input::AbstractArray)
 
-Update an automaton `state` according to the rule `net`. The `state` argument is left
-unmodified, and the updated state is returned.
+Fire a network on an `input` state, and return the result. All arguments are left
+unmodified.
 """
 function fire(net::AbstractAutomaton{T,N}, state::AbstractArray{T,N}) where {T,N}
     fire!(net, state, copy(state))
 end
 
+"""
+    AbstractNeuralNetwork{T,U,N} <: AbstractNetwork
+
+Abstract supertype from which all artificial neural network types inherit
+"""
+abstract type AbstractNeuralNetwork{T,U,N} <: AbstractNetwork end
+
+"""
+    insize(net::AbstractNeuralNetwork)
+
+Get the size of the input array for an artificial neural network
+"""
+insize(net::AbstractNeuralNetwork) = net.insize
+
+"""
+    outsize(net::AbstractNeuralNetwork)
+
+Get the size of the output array for an artificial neural network
+"""
+outsize(net::AbstractNeuralNetwork) = net.outsize
+
+function fire(net::AbstractNeuralNetwork{T,U,N}, input::AbstractArray{T,N}) where {T,U,N}
+    if size(input) != insize(net)
+        throw(BoundsError())
+    end
+    fire!(net, input, Array{U}(outsize(net)...))
+end
+
 include("ca.jl")
+include("neural.jl")
 
 end
---
+++ src/neural.jl
@@ -0,0 +1,48 @@
+"""
+    SimpleANN <: AbstractNeuralNetwork{Int,Int,1}
+
+A simple, 1-layer, feed-forward artificial neural network with perceptron nodes.
+
+    SimpleANN(ws::AbstractArray{Float64,2}, ts::AbstractVector{Float64})
+
+Create a SimpleANN with weights `ws` and thresholds `ts`.
+
+    SimpleANN(n::Int,m::Int) = SimpleANN(zeros(m,n), zeros(m))
+
+Create a SimpleANN with `n` inputs and `m` outputs. All weights and thresholds are
+initialized to zero.
+"""
+struct SimpleANN <: AbstractNeuralNetwork{Int,Int,1}
+    insize :: Int
+    outsize :: Int
+    weights :: Array{Float64,2}
+    thresholds :: Vector{Float64}
+    function SimpleANN(ws::AbstractArray{Float64,2}, ts::AbstractVector{Float64})
+        if any(size(ws).==0)
+            throw(ArgumentError("weights matrix; invalid size"))
+        elseif length(ts) != size(ws,1)
+            throw(DimensionMismatch("weights and thresholds"))
+        end
+        m, n = size(ws)
+        new(n, m, ws, ts)
+    end
+end
+
+SimpleANN(ws::AbstractArray{Float64,2}, ts::AbstractVector{Float64})
+SimpleANN(n::Int,m::Int) = SimpleANN(zeros(m,n), zeros(m))
+
+insize(net::SimpleANN) = (net.insize,)
+outsize(net::SimpleANN) = (net.outsize,)
+
+function fire!(net::SimpleANN, input::Vector{Int}, output::Vector{Int})
+    if net.insize != length(input)
+        throw(DimensionMismatch("invalid input"))
+    elseif net.outsize != length(output)
+        throw(DimensionMismatch("invalid output"))
+    end
+    temp = net.weights * input - net.thresholds
+    for i in 1:net.outsize
+        output[i] = (temp[i] <= 0.) ? 0 : 1
+    end
+    output
+end
